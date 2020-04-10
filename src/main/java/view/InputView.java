eb9d7b26aaea5ccb4140f6ae3d17eaa80026b14d
aegis <aegis1920@gmail.com>
2020-04-10 17:20:33 +0900

    feat : InputView와 OutputView 로직 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,8 +5,29 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
+    public static int inputFunctionNumber() {
+        System.out.println("## 원하는 기능을 선택하세요.");
+        return scanner.nextInt();
+    }
+
     public static int inputTableNumber() {
         System.out.println("## 주문할 테이블을 선택하세요.");
+        return scanner.nextInt();
+    }
+
+    public static int inputMenuNumber() {
+        System.out.println("## 등록할 메뉴를 선택하세요.");
+        return scanner.nextInt();
+    }
+
+    public static int inputCountNumber() {
+        System.out.println("## 주문할 수량을 선택하세요.");
+        return scanner.nextInt();
+    }
+
+    public static int inputPaymentNumber(int tableNumber) {
+        System.out.println(tableNumber + "번 테이블의 결제를 진행합니다.");
+        System.out.println("신용카드는 1번, 현금결제는 2번을 선택하세요");
         return scanner.nextInt();
     }
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,21 +1,23 @@
 package view;
 
-import domain.Menu;
-import domain.Table;
-
 import java.util.List;
+
+import domain.Menu;
+import domain.OrderInfo;
+import domain.Table;
 
 public class OutputView {
     private static final String TOP_LINE = "┌ ─ ┐";
     private static final String TABLE_FORMAT = "| %s |";
     private static final String BOTTOM_LINE = "└ ─ ┘";
+    private static final String BOTTOM_ORDERED_LINE = "└ ₩ ┘";
 
     public static void printTables(final List<Table> tables) {
         System.out.println("## 테이블 목록");
         final int size = tables.size();
         printLine(TOP_LINE, size);
         printTableNumbers(tables);
-        printLine(BOTTOM_LINE, size);
+        printLoopBottomLine(tables);
     }
 
     public static void printMenus(final List<Menu> menus) {
@@ -31,10 +33,46 @@ public class OutputView {
         System.out.println();
     }
 
+    private static void printLoopBottomLine(final List<Table> tables) {
+        for (final Table table : tables) {
+            printBottomLine2(table);
+        }
+        System.out.println();
+    }
+
+    private static void printBottomLine2(final Table table) {
+        if (table.isOrder()) {
+            System.out.printf(BOTTOM_ORDERED_LINE, table);
+            return;
+        }
+        System.out.printf(BOTTOM_LINE, table);
+    }
+
     private static void printTableNumbers(final List<Table> tables) {
         for (final Table table : tables) {
             System.out.printf(TABLE_FORMAT, table);
         }
         System.out.println();
+    }
+
+    public static void printMainInstruction() {
+        System.out.println("## 메인화면");
+        System.out.println("1 - 주문등록");
+        System.out.println("2 - 결제하기");
+        System.out.println("3 - 프로그램 종료");
+    }
+
+    public static void printOrder(final Table table) {
+        System.out.println("## 주문내역");
+        System.out.println("메뉴 수량 금액");
+
+        for (final OrderInfo orderInfo : table.getOrderInfos().getOrderInfos()) {
+            System.out.println(orderInfo.getMenu());
+        }
+    }
+
+    public static void printTotalPrice(final long totalPrice) {
+        System.out.println("## 최종 결제할 금액");
+        System.out.println(totalPrice + "원");
     }
 }
